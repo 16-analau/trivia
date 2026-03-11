c0de467abb8c25407bf53487af29e146fcea5e0e
ana irala <iralapaezanalaura@gmail.com>
2026-03-11 17:24:18 -0300

    se creo la presentacion inicial

---
+++ trivia.py
@@ -0,0 +1,6 @@
+print('                      ''TRIVIA'                '')
+print('               ''Es un juego de TRIVIA''              ')
+
+
+nombre = input('ingrese su nombre')
+print(f'Bienvenido/a {nombre}, suma puntos por cada respuesta correcta :)')
